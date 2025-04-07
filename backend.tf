21d00711d68eac66a922c7f16ed502d160dd0a04
Maruti Chavan <chavan.maruti2307@gmail.com>
2025-04-07 17:45:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-26"
+    bucket = "my-aws-bucket-devops-project"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
-    encrypt = "true"
   }
 }
