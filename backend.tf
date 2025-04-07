9460674929cf87b7417e7146e1e5d8cd4a54fac3
Maruti Chavan <chavan.maruti2307@gmail.com>
2025-04-07 17:56:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-aws-bucket-devops-project"
-    key = "main"
+    key = "eks"
     region = "us-east-1"
     dynamodb_table = "devops-dynamodb-table"
     encrypt = "true"
