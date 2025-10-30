7bdb4f8584e45ef55acadc475f5bed4983236518
Maruti Chavan <chavan.maruti2307@gmail.com>
2025-10-30 22:35:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "my-aws-bucket-devops-project"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "devops-dynamodb-table"
+    dynamodb_table = "my-dynamodb-table"
     use_lockfile = true
   }
 }
